aa329afcf643027b1f08e770d64b5c047335f909
Field Employee <fieldemployee@Rodericks-Work-MacBook-Pro.local>
2020-07-08 21:25:27 -0700

    initial commit, add 2 buttons

---
+++ app/src/main/java/com/example/exampleapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.exampleapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        TextView helloText = (TextView) ((TextView)findViewById(R.id.myTextView)).getText();
+    }
+}
